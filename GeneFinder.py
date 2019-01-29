b94c94675b70f0085bf15625ca6540934e3faeeb
Mario <mcastell@fandm.edu>
2019-01-29 02:16:13 -0500

    01.00.03

--- GeneFinder.py
+++ GeneFinder.py
@@ -12,11 +12,20 @@ DESCRIPTION : The purpose of this file is to be useed as the driver for Load.py 
 
 
 '''
-NAME : codingStrandToAA
-PARAMETERS : string representing dna nucleotide sequence 
-FUNCTION : takes a dna sequence and generates the appropriate Amino acid Sequence  
-RETURN :  String representing the corresponding amino acids 
-TIME COMPLEXITY : theta(n) where n is the size of the dna_seq
+NAME : oneFrame
+PARAMETERS : @param DNA: a string representing a DNA sequence
+FUNCTION : starts at the 0 position of DNA and searches forward 
+           in units of three looking for start codons. When it 
+           finds a start codon, oneFrame should take the slice of 
+           DNA beginning with that "ATG" for the open reading frame
+           that begins there until the stopping codon (TAG, TGA, or TAA). 
+           Adding the open reading frame to a list of reading frames. 
+           If there is no in frame stopping codon then it assumes that the 
+           reading frame extends beyond the given DNA sequence and simply 
+           adds the entire sequence from "ATG" to the end to the reading 
+           frames list. 
+RETURN :  the list of all ORFs found 
+TIME COMPLEXITY : 
 '''
 
 def oneFrame(DNA):
@@ -37,8 +46,37 @@ def oneFrame(DNA):
                 loc += 3
             codon_list.append(curr_Frame)
 
-    print(codon_list)
+    return codon_list
+
+"""
+NAME: oneFrameV2
+PARAMETERS : @param DNA :a string representing a DNA sequence
+FUCNTION : 
+OUTPUT : 
+TIME_COMPLEXITY : 
+"""
+def oneFrameV2(DNA):
+    ORFL = oneFrame(DNA) # Open reading frame list
+    for frame in range(0, len(ORFL)-1, 1): # n33d to check if n3st3d
+        curr_frame = ORFL[frame]
+        next_frame = ORFL[frame+1]
+        curr_frame_count = frame
+        while curr_frame_count < len(ORFL):
+            if len(curr_frame) > len(next_frame):
+
+            curr_frame_count += 1
+
+
+
+
+
+
+
+
+
+
 def main():
+
     dna_seq = "CCCATGTTTTGAAAAATGCCCGGGTAAA"
     dna_seq1 = "CCATGTAGAAATGCCC"
     dna_seq2 = "ATGCCCATGGGGAAATTTTGACCC"
@@ -47,6 +85,6 @@ def main():
     oneFrame(dna_seq1)
     oneFrame(dna_seq2)
 
-
+    oneFrameV2(dna_seq)
 
 main()
